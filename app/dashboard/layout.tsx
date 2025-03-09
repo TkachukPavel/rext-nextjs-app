ff8bc48068b301dc6a43edc8447c3eb87fcd25be
Pavel Tkachuk <pavelt0309@gmail.com>
2025-03-09 22:21:30 +0100

    fix build error

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,4 +1,4 @@
-import SideNav from '../ui/dashboard/sidenav'
+import SideNav from "../ui/dashboard/sidenav"
 
 type Props = {
   children: React.ReactNode
@@ -6,11 +6,11 @@ type Props = {
 
 export default function Layout({ children }: Props) {
   return (
-    <div className='flex h-screen flex-col md:flex-row md:overflow-hidden'>
-      <div className='w-full flex-none md:w-64'>
+    <div className="flex h-screen flex-col md:flex-row md:overflow-hidden">
+      <div className="w-full flex-none md:w-64">
         <SideNav />
       </div>
-      <div className='flex-grow p-6 md:overflow-y-auto md:p-12'>{children}</div>
+      <div className="flex-grow p-6 md:overflow-y-auto md:p-12">{children}</div>
     </div>
   )
 }
